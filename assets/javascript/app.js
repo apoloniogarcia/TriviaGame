659cf133834349ac7fabf9c248fcb39a1fe439fb
Apolonio Garcia <apolonio.garcia2003@gmail.com>
2017-11-30 13:07:23 -0700

    made changes to HW

---
+++ assets/javascript/app.js
@@ -0,0 +1,22 @@
+//Global Variables
+var rightAnswers = 0;
+var wrongAnswers = 0;
+
+//function for starting the game, there has to be an intro with a "Gamestart" function
+
+function startGame(){
+	//once "Start" button is pushed the actual quiz will need to be shown
+}
+
+//each button that is selected will need to be compared to the right answer, 
+//if the button selected is the right answer
+rightAnswers++
+//if the button selected is the wrong answer
+wrongAnswers++
+
+//Once the submit button is pressed, display the final results on screen, replacing the quiz with the results and an option to restart the quiz
+
+function results(){
+	
+}
+
